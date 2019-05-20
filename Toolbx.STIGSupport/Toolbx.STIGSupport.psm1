5f9a94df3a0adcbd49db94ba64334a6a79983546
Zachary Shupp <zachary.shupp@imagine-one.com>
2019-05-20 15:02:56 -0400

    Updated Module load to be recursive

--- Toolbx.STIGSupport/Toolbx.STIGSupport.psm1
+++ Toolbx.STIGSupport/Toolbx.STIGSupport.psm1
@@ -4,13 +4,16 @@ $functionFolders = @('public', 'private')
 ForEach ($folder in $functionFolders) {
     $folderPath = Join-Path -Path "$PSScriptRoot" -ChildPath $folder
     If (Test-Path -Path $folderPath) {
+
         Write-Verbose -Message "Importing from $folder"
-        $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1' 
+
+        $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1' -Recurse
+
         ForEach ($function in $functions) {
             Write-Verbose -Message "  Importing $($function.BaseName)"
             . $($function.FullName)
         }
-    }    
+    }
 }
-$publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\public" -Filter '*.ps1').BaseName
+$publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\public" -Filter '*.ps1' -Recurse).BaseName
 Export-ModuleMember -Function $publicFunctions
