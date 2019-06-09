ead1713c84a7b1956e8c7e18ee1c48727ce69f99
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-09 11:08:03 -0400

    Completed New-Checklist function

--- Toolbx.STIGSupport/public/Checklists/New-Checklist.ps1
+++ Toolbx.STIGSupport/public/Checklists/New-Checklist.ps1
@@ -1,6 +1,31 @@
 
 function New-Checklist {
 
+    <#
+        .SYNOPSIS
+            Creates new STIG Checklists.
+
+        .DESCRIPTION
+            Creates new STIG Checklist from a predefined set within the module. This function also allows users to create STIG Sets so mutltiple STIG Checklists can be created at the same time. If you need to create a STIG Checklist from a template that is not in this module, use the ConvertTo-Checklist function.
+
+        .PARAMETER ChecklistSet
+            Specify a checklist set that will be used to create STIG Checklists for the given host.
+
+        .PARAMETER XCCDFTemplates
+            Specify a XCCDF Template to be used to create a new STIG Checklist. The list of templates are maintained within the module and get updated when new STIG checklists are releasesed.
+
+        .EXAMPLE
+            PS C:\> New-Checklist -Destination C:\Temp\Checklists -ChecklistSet 'Windows 10 - Core'
+
+            This examples shows the function being called to create a set of checklists that are apart of 'Windows 10 - Core'.
+
+        .OUTPUTS
+            fullpaths to checklists that where created. [String[]]
+
+        .NOTES
+            None
+    #>
+
     [CmdletBinding()]
     param (
 
@@ -13,7 +38,6 @@ function New-Checklist {
         [Parameter()]
         [string]
         $HostName = $ENV:COMPUTERNAME
-
     )
 
     DynamicParam {
@@ -28,7 +52,7 @@ function New-Checklist {
         $ParamAttrib.ValueFromPipeline = $ValueFromPipeline.IsPresent
         $ParamAttrib.ValueFromPipelineByPropertyName = $ValueFromPipelineByPropertyName.IsPresent
         $AttribColl.Add($ParamAttrib)
-        $AttribColl.Add( ( New-Object System.Management.Automation.ValidateSetAttribute($($($(Get-Content -Raw -Path "$PSScriptRoot\..\..\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name) + $(if($(Test-Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config") -eq $true){ $(Get-Content -Raw -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name})))))
+        $AttribColl.Add( ( New-Object System.Management.Automation.ValidateSetAttribute($($($(Get-Content -Raw -Path "$PSScriptRoot\..\..\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name) + $(if ($(Test-Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config") -eq $true) { $(Get-Content -Raw -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name })))))
         $RuntimeParam = New-Object System.Management.Automation.RuntimeDefinedParameter('ChecklistSet', [string], $AttribColl)
         $RuntimeParamDic.Add('ChecklistSet', $RuntimeParam)
 
@@ -59,18 +83,26 @@ function New-Checklist {
         # Check is a specific checklist was selected. If so Create that checklist.
         if ($PSBoundParameters.XCCDFTemplates) {
 
-            Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist from $($PSBoundParameters.XCCDFTemplates)"
+            try {
+                Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist from $($PSBoundParameters.XCCDFTemplates)"
 
-            $xccdfTempPath = "$PSScriptRoot\..\..\tools\STIG Data\Current\" + $PSBoundParameters.XCCDFTemplates
+                $xccdfTempPath = "$PSScriptRoot\..\..\tools\STIG Data\Current\" + $PSBoundParameters.XCCDFTemplates
 
 
-            $xccdfNewPath = "$Destination\$($HostName)_$($($PSBoundParameters.XCCDFTemplates).Replace("_Manual-xccdf.xml",".xml"))"
-            Write-Verbose "[$($MyInvocation.MyCommand)] Saving to $xccdfNewPath"
+                $xccdfNewPath = "$Destination\$($HostName)_$($($PSBoundParameters.XCCDFTemplates).Replace("_Manual-xccdf.xml",".xml"))"
+                Write-Verbose "[$($MyInvocation.MyCommand)] Saving to $xccdfNewPath"
 
-            $cklCreated += $xccdfNewPath
+                $cklCreated += $xccdfNewPath
 
-            ConvertTo-Checklist -XccdfPath $xccdfTempPath -Destination $xccdfNewPath
-            Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
+                ConvertTo-Checklist -XccdfPath $xccdfTempPath -Destination $xccdfNewPath
+                Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
+            }
+            catch {
+
+                Write-Error $_
+
+            }
+
         }
 
         # Retrieve Custom Set if provided and create checklists.
@@ -78,17 +110,50 @@ function New-Checklist {
 
             Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist from $($PSBoundParameters.ChecklistSet) Set"
 
-            $PSBoundParameters.ChecklistSet | ForEach-Object {
+            #Declare Config Files with Sets
+            $configs = @()
 
-                $create = $_
-                $ckl = $($templates | Where-Object { $_.Name -like "$create*" }).fullname
-                $xccdfNewPath = "$Destination\$($HostName)_$($($ckl).Replace("_Manual-xccdf.xml",".xml"))"
+            $configs += $(Get-Content -Raw -Path "$PSScriptRoot\..\..\Toolbx.STIGSupport.config" | ConvertFrom-Json)
 
-                ConvertTo-Checklist -XccdfPath $ckl -Destination $xccdfNewPath
+            if ($(Test-Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config") -eq $true -eq $true) {
+                $configs += $(Get-Content -Raw -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config" | ConvertFrom-Json)
+            }
 
-                Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
+            foreach ($c in $configs) {
 
-                $cklCreated += $xccdfNewPath
+                foreach ($o in $c.NewCheckListOptions) {
+
+                    if ($o.Name -eq $PSBoundParameters.ChecklistSet) {
+
+                        foreach ($ckl in $o.ckl) {
+
+                            Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist $ckl"
+
+                            try {
+
+                                $checklist = $($templates | Where-Object { $_.Name -like "$ckl*" })
+                                $xccdfNewPath = "$Destination\$($HostName)_$($($checklist.name).Replace("_Manual-xccdf.xml",".xml"))"
+
+                                Write-Debug "[$($MyInvocation.MyCommand)] Xccdf: $($checklist.fullname)"
+                                Write-Debug "[$($MyInvocation.MyCommand)] SaveTo: $xccdfNewPath"
+                                ConvertTo-Checklist -XccdfPath $checklist.fullname -Destination $xccdfNewPath
+
+                                Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
+                                $cklCreated += $xccdfNewPath
+
+                            }
+                            catch {
+
+                                Write-Error $_
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
             }
 
         }
