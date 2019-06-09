5774f820ff9cba4cb4090556ef9ea481d3eb4b68
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-08 20:42:51 -0400

    Update New-Checklist with CustomCheclists

--- Toolbx.STIGSupport/public/Checklists/New-Checklist.ps1
+++ Toolbx.STIGSupport/public/Checklists/New-Checklist.ps1
@@ -1,3 +1,4 @@
+
 function New-Checklist {
 
     [CmdletBinding()]
@@ -11,51 +12,15 @@ function New-Checklist {
         # Specify the HostName for the new checklist(s). If no name is provided, the local computername will be used.
         [Parameter()]
         [string]
-        $HostName = $ENV:COMPUTERNAME,
+        $HostName = $ENV:COMPUTERNAME
 
-        # Will Create Server 2012 R2 Member OS, .Net Framework 4, and IE11 Checklists.
-        [Parameter()]
-        [Switch]
-        $Win2012R2MSCore,
-
-        # Will Create Server 2012 R2 Domain Controller OS, .Net Framework 4, and IE11 Checklists.
-        [Parameter()]
-        [Switch]
-        $Win2012R2DCCore,
-
-        # Will Create Server 2016 OS, .Net Framework 4, and IE11 Checklists.
-        [Parameter()]
-        [Switch]
-        $Win2016Core,
-
-        # Will Create Windows 10 OS, .Net Framework 4, and IE11, Adobe Reader Continous, Chrome, Firefox, and Java 8 Checklists
-        [Parameter()]
-        [Switch]
-        $Win10Core,
-
-        # Will Create Adobe Reader Continous Checklist.
-        [Parameter()]
-        [Switch]
-        $AdobeReaderDC,
-
-        # Will Create Win JRE Checklist.
-        [Parameter()]
-        [Switch]
-        $WinJRE,
-
-        # Will Create Unix JRE Checklist.
-        [Parameter()]
-        [Switch]
-        $UnixJRE,
-
-        # Will Create RedHat 6.
-        [Parameter()]
-        [Switch]
-        $Office2010
     )
 
     DynamicParam {
+
+        #Create Parameter Dictionary
         $RuntimeParamDic = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
         $AttribColl = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
         $ParamAttrib = New-Object System.Management.Automation.ParameterAttribute
         $ParamAttrib.Mandatory = $Mandatory.IsPresent
@@ -63,9 +28,21 @@ function New-Checklist {
         $ParamAttrib.ValueFromPipeline = $ValueFromPipeline.IsPresent
         $ParamAttrib.ValueFromPipelineByPropertyName = $ValueFromPipelineByPropertyName.IsPresent
         $AttribColl.Add($ParamAttrib)
-        $AttribColl.Add((New-Object System.Management.Automation.ValidateSetAttribute((Get-ChildItem $("$PSScriptRoot\..\..\tools\STIG Data\Current") -File | Select-Object -ExpandProperty Name))))
-        $RuntimeParam = New-Object System.Management.Automation.RuntimeDefinedParameter('XCCDFTemplates', [string], $AttribColl)
-        $RuntimeParamDic.Add('XCCDFTemplates', $RuntimeParam)
+        $AttribColl.Add( ( New-Object System.Management.Automation.ValidateSetAttribute($($($(Get-Content -Raw -Path "$PSScriptRoot\..\..\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name) + $(if($(Test-Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config") -eq $true){ $(Get-Content -Raw -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Toolbx.STIGSupport.config" | ConvertFrom-Json).NewCheckListOptions.Name})))))
+        $RuntimeParam = New-Object System.Management.Automation.RuntimeDefinedParameter('ChecklistSet', [string], $AttribColl)
+        $RuntimeParamDic.Add('ChecklistSet', $RuntimeParam)
+
+        $AttribColl1 = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $ParamAttrib1 = New-Object System.Management.Automation.ParameterAttribute
+        $ParamAttrib1.Mandatory = $Mandatory.IsPresent
+        $ParamAttrib1.ParameterSetName = '__AllParameterSets'
+        $ParamAttrib1.ValueFromPipeline = $ValueFromPipeline.IsPresent
+        $ParamAttrib1.ValueFromPipelineByPropertyName = $ValueFromPipelineByPropertyName.IsPresent
+        $AttribColl1.Add($ParamAttrib1)
+        $AttribColl1.Add((New-Object System.Management.Automation.ValidateSetAttribute((Get-ChildItem $("$PSScriptRoot\..\..\tools\STIG Data\Current") -File | Select-Object -ExpandProperty Name))))
+        $RuntimeParam1 = New-Object System.Management.Automation.RuntimeDefinedParameter('XCCDFTemplates', [string], $AttribColl1)
+        $RuntimeParamDic.Add('XCCDFTemplates', $RuntimeParam1)
+
         return $RuntimeParamDic
     }
 
@@ -96,47 +73,26 @@ function New-Checklist {
             Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
         }
 
-        [Hashtable]$core = @{ }
+        # Retrieve Custom Set if provided and create checklists.
+        if ($PSBoundParameters.ChecklistSet) {
 
-        If ($Win10Core) { $core.Add("WIN10", $($templates | Where-Object { $_.Name -like "U_MS_Windows_10_STIG_*" }).fullname) }
+            Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist from $($PSBoundParameters.ChecklistSet) Set"
 
-        If ($Win10Core -or $Win2012R2MSCore -or $Win2012R2DCCore -or $Win2016Core) {
-            $core.Add("IE11", $($templates | Where-Object { $_.Name -like "U_MS_IE11_STIG_*" }).fullname)
+            $PSBoundParameters.ChecklistSet | ForEach-Object {
+
+                $create = $_
+                $ckl = $($templates | Where-Object { $_.Name -like "$create*" }).fullname
+                $xccdfNewPath = "$Destination\$($HostName)_$($($ckl).Replace("_Manual-xccdf.xml",".xml"))"
+
+                ConvertTo-Checklist -XccdfPath $ckl -Destination $xccdfNewPath
+
+                Write-Verbose "[$($MyInvocation.MyCommand)] Created $xccdfNewPath"
+
+                $cklCreated += $xccdfNewPath
+            }
+
         }
 
-        If ($Win10Core) { $core.Add("Chrome", $($templates | Where-Object { $_.Name -like "U_Google_Chrome_STIG_*" }).fullname) }
-
-        If ($Win10Core -or $AdobeReaderDC) { $core.Add("ARC", $($templates | Where-Object { $_.Name -like "U_Adobe_Acrobat_Reader_DC_Continuous_*" }).fullname) }
-
-        If ($Win10Core -or $Win2012R2MSCore -or $Win2012R2DCCore -or $Win2016Core) {
-            $core.Add("DNF", $($templates | Where-Object { $_.Name -like "U_MS_DotNet_Framework_4-0_STIG_*" }).fullname)
-        }
-
-        If ($Win10Core) { $core.Add("FF", $($templates | Where-Object { $_.Name -like "U_Mozilla_FireFox_STIG_*" }).fullname) }
-
-        If ($Win10Core -or $WinJRE) { $core.Add("WinJRE8", $($templates | Where-Object { $_.Name -like "U_Oracle_JRE_8_Windows_STIG_*" }).fullname) }
-
-        If ($Win2012R2MSCore) { $core.Add("2k12MS", $($templates | Where-Object { $_.Name -like "U_MS_Windows_2012_and_2012_R2_MS_STIG_*" }).fullname) }
-
-        If ($Win2012R2DCCore) { $core.Add("2k12DC", $($templates | Where-Object { $_.Name -like "U_MS_Windows_2012_and_2012_R2_DC_STIG_*" }).fullname) }
-
-        If ($Win2016Core) { $core.Add("2k16", $($templates | Where-Object { $_.Name -like "U_MS_Windows_Server_2016_STIG_*" }).fullname) }
-
-        # Create Checklists
-        foreach ($key in $core.keys) {
-
-            Write-Verbose "[$($MyInvocation.MyCommand)] Creating New Checklist from $(split-path $core[$key] -leaf)"
-
-            $saveTo = "$Destination\$($HostName)_$($($(split-path $core[$key] -leaf)).Replace("_Manual-xccdf.xml",".xml"))"
-
-            ConvertTo-Checklist -XccdfPath $core[$key] -Destination $saveTo -HostName $HostName
-
-            $cklCreated += $saveTo
-
-            Write-Verbose "[$($MyInvocation.MyCommand)] Created $saveTo"
-        }
-
-        $cklCreated
     }
 
 }
