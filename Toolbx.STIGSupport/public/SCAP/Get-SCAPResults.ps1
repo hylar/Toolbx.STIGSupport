24ae4dc7b6b2ff9335b3573cab1f1adc490d6ebd
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-13 16:45:46 -0400

    Updated Get-SCAPResults help

--- Toolbx.STIGSupport/public/SCAP/Get-SCAPResults.ps1
+++ Toolbx.STIGSupport/public/SCAP/Get-SCAPResults.ps1
@@ -1,6 +1,23 @@
-
-
 function Get-SCAPResults {
+
+    <#
+        .SYNOPSIS
+            Get Results from SCAP XCCDF Results.
+
+        .DESCRIPTION
+              Get Results from SCAP XCCDF Results.
+
+        .EXAMPLE
+            PS C:\>$resluts = Get-SCAPResults -XccdfPath 'C:\Temp\Windows10-SCAP-XCCDF.xml'
+
+            This examples shows calling the command and saving the results to $results.
+
+        .OUTPUTS
+            PSOBJECT
+
+        .NOTES
+            None
+    #>
 
     Param(
 
