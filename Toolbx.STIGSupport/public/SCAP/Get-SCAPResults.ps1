2f3df3d028ca791de8fc1115cf4fd4993c29d68c
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-13 16:37:08 -0400

    Renamed Get-SCAPXccdfResults.ps1

--- Toolbx.STIGSupport/public/SCAP/Get-SCAPResults.ps1
+++ Toolbx.STIGSupport/public/SCAP/Get-SCAPResults.ps1
@@ -1,17 +1,17 @@
 
 
-function Get-SCAPXccdfResults {
+function Get-SCAPResults {
 
     Param(
 
         [Parameter(Mandatory = $true)]
-        [ValidateScript( {Test-Path -Path $_})]
+        [ValidateScript( { Test-Path -Path $_ })]
         [string]
-        $Path
+        $XccdfPath
 
     )
 
-    $XCCDF = [XML](Get-Content -Encoding UTF8 -Path $Path)
+    $XCCDF = [XML](Get-Content -Encoding UTF8 -Path $XccdfPath)
 
     #Grab rule results
     $results = $XCCDF.Benchmark.TestResult.'rule-result'
@@ -21,9 +21,7 @@ function Get-SCAPXccdfResults {
     foreach ($result in $results) {
 
         #Get IP
-        if ($result.idref -match "(SV-.*_rule)") {
-            $result.idref = $Matches[1]
-        }
+        if ($result.idref -match "(SV-.*_rule)") { $result.idref = $Matches[1] }
 
         #Return ID and result
         $toReturn += New-Object PSObject -Property @{RuleID = $result.idref; Result = $result.result }
