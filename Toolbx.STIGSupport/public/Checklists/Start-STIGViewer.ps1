5eaee281bae49f88550d1afc6fdb02029ffbb173
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-15 11:32:53 -0400

    updated help for Start-STIGViewer

--- Toolbx.STIGSupport/public/Checklists/Start-STIGViewer.ps1
+++ Toolbx.STIGSupport/public/Checklists/Start-STIGViewer.ps1
@@ -1,13 +1,34 @@
 Function Start-STIGViewer {
 
+    <#
+        .SYNOPSIS
+            Launch new instance of DISA STIG Viewer
+
+        .DESCRIPTION
+             Launch new instance of DISA STIG Viewer
+
+        .EXAMPLE
+            PS C:\> Start-STIGViewer
+
+        .OUTPUTS
+            None
+
+        .NOTES
+            None
+    #>
+
     [CmdletBinding()]
     param()
 
     Try {
+
         Start-Process -FilePath .\Toolbx.STIGSupport\tools\STIGViewer-2.9.jar
+
     }
     Catch {
+
         $PSCmdlet.ThrowTerminatingError( $_ )
+
     }
 
 }
