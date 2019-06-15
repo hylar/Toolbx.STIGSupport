76e0640d25255a9a0f5cf52f57218426b171768a
Zachary Shupp <zacharyshupp@users.noreply.github.com>
2019-06-15 10:51:41 -0400

    Added Help to Export-Checklist

--- Toolbx.STIGSupport/public/Checklists/Export-Checklist.ps1
+++ Toolbx.STIGSupport/public/Checklists/Export-Checklist.ps1
@@ -1,7 +1,27 @@
 Function Export-Checklist {
 
+    <#
+        .SYNOPSIS
+            Export STIG Checklist
+
+        .DESCRIPTION
+            Export STIG Checklist
+
+        .EXAMPLE
+            PS C:\> Export-Checklist -Checklist $CKL -path C:\Temp\U_Windows10.ckl
+
+            This example shows how to Export a checklist. $CKL is passed with the xml data and then it is saved to the location listed in Path.
+
+        .OUTPUTS
+            [XML]
+
+        .NOTES
+            None
+    #>
+
     [CmdletBinding()]
     Param(
+
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [XML]
         $Checklist,
@@ -9,6 +29,7 @@ Function Export-Checklist {
         [Parameter(Mandatory = $true)]
         [string]
         $Path
+
     )
 
     $XMLSettings = New-Object -TypeName System.XML.XMLWriterSettings
@@ -20,8 +41,8 @@ Function Export-Checklist {
 
     $XMLWriter = [System.XML.XMLTextWriter]::Create($Path, $XMLSettings)
 
-    $Global:Test=$Checklist
-    $Checklist.InnerXml=$Checklist.InnerXml -replace "&#x0;","[0x00]"
+    $Checklist.InnerXml = $Checklist.InnerXml -replace "&#x0;", "[0x00]"
+
     $Checklist.Save($XMLWriter)
     $XMLWriter.Flush()
     $XMLWriter.Dispose();
